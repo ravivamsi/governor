99d11cd8fb677a098785008eea00593fff4941d4
Vamsi Ravi <ravivamsi@yahoo.com>
2019-12-19 23:23:42 -0600

    Updated Vital Models

--- src/main/java/com/servec/governor/models/Health.java
+++ src/main/java/com/servec/governor/models/Health.java
@@ -17,142 +17,170 @@ import io.swagger.annotations.ApiModelProperty;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-15T06:26:02.812Z")
 
 public class Health {
-	@JsonProperty("version")
-	private String version = null;
+	  @JsonProperty("version")
+	  private String version = null;
 
-	@JsonProperty("appname")
-	private String appname = null;
+	  @JsonProperty("appname")
+	  private String appname = null;
 
-	@JsonProperty("buildtime")
-	private String buildtime = null;
+	  @JsonProperty("buildtime")
+	  private String buildtime = null;
 
-	@JsonProperty("releasetime")
-	private String releasetime = null;
+	  @JsonProperty("releasetime")
+	  private String releasetime = null;
 
-	public Health version(String version) {
-		this.version = version;
-		return this;
-	}
+	  @JsonProperty("buildnumber")
+	  private String buildnumber = null;
 
-	/**
-	 * Get version
-	 * 
-	 * @return version
-	 **/
-	@ApiModelProperty(required = true, value = "")
-	@NotNull
+	  public Health version(String version) {
+	    this.version = version;
+	    return this;
+	  }
 
-	public String getVersion() {
-		return version;
-	}
+	  /**
+	   * Get version
+	   * @return version
+	  **/
+	  @ApiModelProperty(required = true, value = "")
+	  @NotNull
 
-	public void setVersion(String version) {
-		this.version = version;
-	}
 
-	public Health appname(String appname) {
-		this.appname = appname;
-		return this;
-	}
+	  public String getVersion() {
+	    return version;
+	  }
 
-	/**
-	 * Get appname
-	 * 
-	 * @return appname
-	 **/
-	@ApiModelProperty(required = true, value = "")
-	@NotNull
+	  public void setVersion(String version) {
+	    this.version = version;
+	  }
 
-	public String getAppname() {
-		return appname;
-	}
+	  public Health appname(String appname) {
+	    this.appname = appname;
+	    return this;
+	  }
 
-	public void setAppname(String appname) {
-		this.appname = appname;
-	}
+	  /**
+	   * Get appname
+	   * @return appname
+	  **/
+	  @ApiModelProperty(required = true, value = "")
+	  @NotNull
 
-	public Health buildtime(String buildtime) {
-		this.buildtime = buildtime;
-		return this;
-	}
 
-	/**
-	 * Get buildtime
-	 * 
-	 * @return buildtime
-	 **/
-	@ApiModelProperty(required = true, value = "")
-	@NotNull
+	  public String getAppname() {
+	    return appname;
+	  }
 
-	public String getBuildtime() {
-		return buildtime;
-	}
+	  public void setAppname(String appname) {
+	    this.appname = appname;
+	  }
 
-	public void setBuildtime(String buildtime) {
-		this.buildtime = buildtime;
-	}
+	  public Health buildtime(String buildtime) {
+	    this.buildtime = buildtime;
+	    return this;
+	  }
 
-	public Health releasetime(String releasetime) {
-		this.releasetime = releasetime;
-		return this;
-	}
+	  /**
+	   * Get buildtime
+	   * @return buildtime
+	  **/
+	  @ApiModelProperty(required = true, value = "")
+	  @NotNull
 
-	/**
-	 * Get releasetime
-	 * 
-	 * @return releasetime
-	 **/
-	@ApiModelProperty(required = true, value = "")
-	@NotNull
 
-	public String getReleasetime() {
-		return releasetime;
-	}
+	  public String getBuildtime() {
+	    return buildtime;
+	  }
 
-	public void setReleasetime(String releasetime) {
-		this.releasetime = releasetime;
-	}
+	  public void setBuildtime(String buildtime) {
+	    this.buildtime = buildtime;
+	  }
 
-	@Override
-	public boolean equals(java.lang.Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		Health health = (Health) o;
-		return Objects.equals(this.version, health.version) && Objects.equals(this.appname, health.appname)
-				&& Objects.equals(this.buildtime, health.buildtime)
-				&& Objects.equals(this.releasetime, health.releasetime);
-	}
+	  public Health releasetime(String releasetime) {
+	    this.releasetime = releasetime;
+	    return this;
+	  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(version, appname, buildtime, releasetime);
-	}
+	  /**
+	   * Get releasetime
+	   * @return releasetime
+	  **/
+	  @ApiModelProperty(required = true, value = "")
+	  @NotNull
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class Health {\n");
 
-		sb.append("    version: ").append(toIndentedString(version)).append("\n");
-		sb.append("    appname: ").append(toIndentedString(appname)).append("\n");
-		sb.append("    buildtime: ").append(toIndentedString(buildtime)).append("\n");
-		sb.append("    releasetime: ").append(toIndentedString(releasetime)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
+	  public String getReleasetime() {
+	    return releasetime;
+	  }
 
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces
-	 * (except the first line).
-	 */
-	private String toIndentedString(java.lang.Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
+	  public void setReleasetime(String releasetime) {
+	    this.releasetime = releasetime;
+	  }
+
+	  public Health buildnumber(String buildnumber) {
+	    this.buildnumber = buildnumber;
+	    return this;
+	  }
+
+	  /**
+	   * Get buildnumber
+	   * @return buildnumber
+	  **/
+	  @ApiModelProperty(required = true, value = "")
+	  @NotNull
+
+
+	  public String getBuildnumber() {
+	    return buildnumber;
+	  }
+
+	  public void setBuildnumber(String buildnumber) {
+	    this.buildnumber = buildnumber;
+	  }
+
+
+	  @Override
+	  public boolean equals(java.lang.Object o) {
+	    if (this == o) {
+	      return true;
+	    }
+	    if (o == null || getClass() != o.getClass()) {
+	      return false;
+	    }
+	    Health health = (Health) o;
+	    return Objects.equals(this.version, health.version) &&
+	        Objects.equals(this.appname, health.appname) &&
+	        Objects.equals(this.buildtime, health.buildtime) &&
+	        Objects.equals(this.releasetime, health.releasetime) &&
+	        Objects.equals(this.buildnumber, health.buildnumber);
+	  }
+
+	  @Override
+	  public int hashCode() {
+	    return Objects.hash(version, appname, buildtime, releasetime, buildnumber);
+	  }
+
+	  @Override
+	  public String toString() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("class Health {\n");
+	    
+	    sb.append("    version: ").append(toIndentedString(version)).append("\n");
+	    sb.append("    appname: ").append(toIndentedString(appname)).append("\n");
+	    sb.append("    buildtime: ").append(toIndentedString(buildtime)).append("\n");
+	    sb.append("    releasetime: ").append(toIndentedString(releasetime)).append("\n");
+	    sb.append("    buildnumber: ").append(toIndentedString(buildnumber)).append("\n");
+	    sb.append("}");
+	    return sb.toString();
+	  }
+
+	  /**
+	   * Convert the given object to string with each line indented by 4 spaces
+	   * (except the first line).
+	   */
+	  private String toIndentedString(java.lang.Object o) {
+	    if (o == null) {
+	      return "null";
+	    }
+	    return o.toString().replace("\n", "\n    ");
+	  }
 }
---
+++ src/main/java/com/servec/governor/models/Status.java
@@ -0,0 +1,181 @@
+package com.servec.governor.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * Status
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-20T05:19:56.685Z")
+
+public class Status   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  /**
+   * Gets or Sets overallstatus
+   */
+  public enum OverallstatusEnum {
+    UP("up"),
+    
+    DOWN("down"),
+    
+    UNKNOWN("unknown");
+
+    private String value;
+
+    OverallstatusEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static OverallstatusEnum fromValue(String text) {
+      for (OverallstatusEnum b : OverallstatusEnum.values()) {
+        if (String.valueOf(b.value).equals(text)) {
+          return b;
+        }
+      }
+      return null;
+    }
+  }
+
+  @JsonProperty("overallstatus")
+  private OverallstatusEnum overallstatus = null;
+
+  @JsonProperty("dependencies")
+  @Valid
+  private List<Dependency> dependencies = null;
+
+  public Status id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(required = true, value = "")
+  @NotNull
+
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Status overallstatus(OverallstatusEnum overallstatus) {
+    this.overallstatus = overallstatus;
+    return this;
+  }
+
+  /**
+   * Get overallstatus
+   * @return overallstatus
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public OverallstatusEnum getOverallstatus() {
+    return overallstatus;
+  }
+
+  public void setOverallstatus(OverallstatusEnum overallstatus) {
+    this.overallstatus = overallstatus;
+  }
+
+  public Status dependencies(List<Dependency> dependencies) {
+    this.dependencies = dependencies;
+    return this;
+  }
+
+  public Status addDependenciesItem(Dependency dependenciesItem) {
+    if (this.dependencies == null) {
+      this.dependencies = new ArrayList<Dependency>();
+    }
+    this.dependencies.add(dependenciesItem);
+    return this;
+  }
+
+  /**
+   * Get dependencies
+   * @return dependencies
+  **/
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<Dependency> getDependencies() {
+    return dependencies;
+  }
+
+  public void setDependencies(List<Dependency> dependencies) {
+    this.dependencies = dependencies;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Status status = (Status) o;
+    return Objects.equals(this.id, status.id) &&
+        Objects.equals(this.overallstatus, status.overallstatus) &&
+        Objects.equals(this.dependencies, status.dependencies);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, overallstatus, dependencies);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Status {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    overallstatus: ").append(toIndentedString(overallstatus)).append("\n");
+    sb.append("    dependencies: ").append(toIndentedString(dependencies)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
